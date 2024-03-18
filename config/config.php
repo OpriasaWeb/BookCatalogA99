ea7d8245f466b119eafdd1e280a83314997a4b7a
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-03-18 22:03:41 +0800

    feat: config database and base url

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+class ProjectConfig{
+  static $book = array(
+    "host" => "localhost",
+    "port" => "3306",
+    "user" => "root",
+    "password" => "",
+  );
+
+  static $baseurl = "http://localhost/bookcatalog/";
+
+}
+
+
+?>
